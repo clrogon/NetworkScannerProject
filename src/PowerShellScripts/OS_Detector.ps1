0b63cb05222de63ee2747f0809667965024c388c
Cláudio Roberto Gonçalves <clrogon@gmail.com>
2023-10-02 22:53:27 +0100

    Update OS_Detector.ps1

--- src/PowerShellScripts/OS_Detector.ps1
+++ src/PowerShellScripts/OS_Detector.ps1
@@ -1,1 +1,6 @@
+param(
+    [string]$computerName = "localhost"
+)
 
+$os = Get-WmiObject -Class Win32_OperatingSystem -ComputerName $computerName
+Write-Output "OS: $($os.Caption) - $($os.Version)"
