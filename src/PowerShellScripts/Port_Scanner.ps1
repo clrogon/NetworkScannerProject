5948243e80ffd9913b30670fdf36ff559784f3c8
Cláudio Roberto Gonçalves <clrogon@gmail.com>
2023-10-02 22:55:34 +0100

    Update Port_Scanner.ps1

--- src/PowerShellScripts/Port_Scanner.ps1
+++ src/PowerShellScripts/Port_Scanner.ps1
@@ -1,1 +1,14 @@
+param(
+    [string]$computerName = "localhost",
+    [int[]]$ports = @(80, 443)
+)
 
+$ports | ForEach-Object {
+    $port = $_
+    $result = Test-NetConnection -ComputerName $computerName -Port $port
+    if ($result.TcpTestSucceeded) {
+        Write-Output "Port $port is open on $computerName"
+    } else {
+        Write-Output "Port $port is closed on $computerName"
+    }
+}
