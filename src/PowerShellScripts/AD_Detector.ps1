733b25f8d635c8f660d402d9a818af4a1177e5d3
Cláudio Roberto Gonçalves <clrogon@gmail.com>
2023-10-02 22:54:38 +0100

    Update AD_Detector.ps1

--- src/PowerShellScripts/AD_Detector.ps1
+++ src/PowerShellScripts/AD_Detector.ps1
@@ -1,1 +1,6 @@
-
+try {
+    $domainController = Get-ADDomainController -Discover
+    Write-Output "Active Directory Domain Controller: $($domainController.HostName)"
+} catch {
+    Write-Output "No Active Directory Domain Controller found."
+}
