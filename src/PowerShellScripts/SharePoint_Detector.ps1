edd8170137cb77e19fe8f8fd0bfea16bcee75269
Cláudio Roberto Gonçalves <clrogon@gmail.com>
2023-10-02 22:55:08 +0100

    Update SharePoint_Detector.ps1

--- src/PowerShellScripts/SharePoint_Detector.ps1
+++ src/PowerShellScripts/SharePoint_Detector.ps1
@@ -1,1 +1,6 @@
-
+try {
+    Import-Module Microsoft.SharePoint.PowerShell -ErrorAction Stop
+    Write-Output "SharePoint module loaded successfully."
+} catch {
+    Write-Output "SharePoint module could not be loaded."
+}
